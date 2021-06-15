687eec8d35bb19f3f3b67f74ccbab81dd8d8df74
YasumoAkuto <67bread@gmail.com>
2021-06-15 19:35:19 +0900

    6/15 鬼のプレイヤーを見つけるスクリプトの更新（加藤）

--- Assets/Photon/PhotonUnityNetworking/Resources/Catch_player.cs
+++ Assets/Photon/PhotonUnityNetworking/Resources/Catch_player.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Catch_player : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Camera demon_cam;
     private RaycastHit hit; //Raycastの情報を取得するための構造体
+    Rect rect = new Rect(0, 0, 1, 1);
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerStay(Collider hako)   //コライダーの中にいるとき呼び出される関数
@@ -22,22 +24,27 @@ public class Catch_player : MonoBehaviour
         if (hako.gameObject.CompareTag("Player"))   //コライダーのtagがPlayerであるとき
         {
             GameObject Target = GameObject.Find($"{hako.name}");    //Playerの名前を取得
-            var diff = Target.transform.position - transform.position;  //プレイヤーと鬼の距離を取得(Vector3)
+            var diff = Target.transform.position - demon_cam.transform.position;  //プレイヤーと鬼の距離を取得(Vector3)
             var distance = diff.magnitude;  //Vector3の大きさ
             var direction = diff.normalized;    //Vector3の向き
+            var viewportPos = demon_cam.WorldToViewportPoint(Target.transform.position);
 
-            if(Physics.Raycast(transform.position, direction, out hit, distance))   //RaycastをPlayer方向に飛ばす
+            if(rect.Contains(viewportPos))
             {
-                if(hit.transform.gameObject == Target)  //軌道上にPlayerがいるとき
+                if(Physics.Raycast(demon_cam.transform.position, direction, out hit, distance))   //RaycastをPlayer方向に飛ばす
                 {
-                    Debug.Log("見つけた");
-                }
-                else    //いないとき
-                {
-                    Debug.Log("見つけてない");
-                }
+                    if(hit.transform.gameObject == Target)  //軌道上にPlayerがいるとき
+                    {
+                        Debug.Log("見つけた");
+                    }
+                    else Debug.Log("見つけてない");
 
+                }
             }
+            else{
+                Debug.Log("画面外だよ");
+            }
+
         }
     }
 }
