914eb82b78bdcf47c8e5aebb869be1cf3f3cf7ba
EZAYUKI <ezaezaezawa@ezweb.ne.jp>
2021-07-20 19:40:46 +0900

    データベースとステージを平らにした

--- Assets/Photon/PhotonUnityNetworking/Resources/ItemDataBase.cs
+++ Assets/Photon/PhotonUnityNetworking/Resources/ItemDataBase.cs
@@ -5,18 +5,45 @@ public class ItemDataBase : MonoBehaviour {
  
     //　アイテムの種類
     public enum ItemType {
-        Sword,
-        Gun,
+        Wood,
+        himo,
+        branch,
+
+        vine,
+
+        leaf,
+
+        watch,
+
+        transparent,
+
+        scapegoat,
+
+        catcheye,
+
+        dan,
+
+        stone,
+
         Other
+
     }
     //　アイテムデータのリスト
     public List<ItemData> itemDataList = new List<ItemData>(); 
  
     void Awake() {
         //　アイテムの全情報を作成
-        itemDataList.Add(new ItemData(Resources.Load("Sword", typeof(Sprite)) as Sprite, "FlashLight", ItemType.Sword, "あれば便利な辺りを照らすライト"));
-        itemDataList.Add(new ItemData(Resources.Load("Sword", typeof(Sprite)) as Sprite, "BroadSword", ItemType.Sword, "普通の剣"));
-        itemDataList.Add(new ItemData(Resources.Load("Gun", typeof(Sprite)) as Sprite, "HandGun", ItemType.Gun, "標準のハンドガン"));
+        itemDataList.Add(new ItemData(Resources.Load("Wood", typeof(Sprite)) as Sprite, "木材", ItemType.Wood, "ツリーハウスを直すことができる"));
+        itemDataList.Add(new ItemData(Resources.Load("rope", typeof(Sprite)) as Sprite, "ひも", ItemType.himo, "ツリーハウスを直すことができる"));
+        itemDataList.Add(new ItemData(Resources.Load("branch", typeof(Sprite)) as Sprite, "枝", ItemType.branch, "ツリーハウスを直すことができる"));
+        itemDataList.Add(new ItemData(Resources.Load("vine", typeof(Sprite)) as Sprite, "つる", ItemType.vine, "ツリーハウスを直すことできる"));
+        itemDataList.Add(new ItemData(Resources.Load("leaf", typeof(Sprite)) as Sprite, "葉", ItemType.leaf, "ギリースーツを作成できる"));
+        itemDataList.Add(new ItemData(Resources.Load("watch", typeof(Sprite)) as Sprite, "腕時計", ItemType.watch, "残り時間が分かる"));
+        itemDataList.Add(new ItemData(Resources.Load("transparent", typeof(Sprite)) as Sprite, "透明化", ItemType.transparent, "３０秒透明になる"));
+        itemDataList.Add(new ItemData(Resources.Load("scapegoat", typeof(Sprite)) as Sprite, "スケープゴート", ItemType.scapegoat, "残り時間が分かる"));
+        itemDataList.Add(new ItemData(Resources.Load("catcheye", typeof(Sprite)) as Sprite, "キャッチアイ", ItemType.catcheye, "残り時間が分かる"));
+        itemDataList.Add(new ItemData(Resources.Load("dan", typeof(Sprite)) as Sprite, "段ボール", ItemType.dan, "持ち運び隠れられる"));
+        itemDataList.Add(new ItemData(Resources.Load("stone", typeof(Sprite)) as Sprite, "石", ItemType.stone, "投げることで敵の意識がそちらに向く"));
  
     }
     //　全アイテムデータを返す
