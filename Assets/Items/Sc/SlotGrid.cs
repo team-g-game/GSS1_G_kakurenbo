b3501eb66c9fe240f023a4d06a24c2ae85d7c023
EZAYUKI <ezaezaezawa@ezweb.ne.jp>
2021-08-19 06:52:17 +0900

    8/19　持ち物画面完成

---
+++ Assets/Items/Sc/SlotGrid.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotGrid : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject slotPrefab;
+
+    private int slotNumber = 15;
+
+    [SerializeField]
+    private Item[] allItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < slotNumber; i++)
+        {
+            GameObject slotObj = Instantiate(slotPrefab, this.transform);
+        
+            Slot slot = slotObj.GetComponent<Slot>();
+
+            if (i<allItems.Length)
+            {
+                slot.SetItem(allItems[i]);
+            }
+            else
+            {
+                slot.SetItem(null);
+            }
+        }
+    }
+
+
+}
