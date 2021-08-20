a60d4f73801a6eb0933d1b44fc943a7435e7ec6f
EZAYUKI <ezaezaezawa@ezweb.ne.jp>
2021-08-21 01:27:31 +0900

    Titleに悪ガキと鬼の変数入れた

---
+++ Assets/var3/Script/Title_button_choise.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Title_button_choise : MonoBehaviour {
+ 
+    // Use this for initialization
+    void Start () {
+         
+    }
+     
+    // Update is called once per frame
+    void Update () {
+         
+    }
+ 
+    //ボタンを押した時の処理
+    public void Click()
+    {
+        int Demon;
+        Demon = 1;
+        //ログ出力
+        Debug.Log(Demon);
+    }
+}
