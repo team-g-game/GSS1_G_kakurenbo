37ca791ce1f70279103ac03eb930facc43a19c58
EZAYUKI <ezaezaezawa@ezweb.ne.jp>
2021-08-22 22:59:04 +0900

    08/22 しげみのコライダー位置とタイトルSC修正

--- Assets/var3/Script/Title_button_choise.cs
+++ Assets/var3/Script/Title_button_choise.cs
@@ -15,11 +15,19 @@ public class Title_button_choise : MonoBehaviour {
     }
  
     //ボタンを押した時の処理
-    public void Click()
+    public void Click_Demon()
     {
         int Demon;
         Demon = 1;
         //ログ出力
         Debug.Log(Demon);
+    }
+
+    public void Click_warugi()
+    {
+        int gaki;
+        gaki = 0;
+        //ログ出力
+        Debug.Log(gaki);
     }
 }
